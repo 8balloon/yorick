7fc203ba3338af33a02939bb2f0f3a6f742c3036
Ethan Gregory Clark <eclark@medallia.com>
2021-03-09 16:27:09 -0800

    testable runnerconfig

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-module.exports = require('./lib/YorickRunner')
+const { YorickRunner } = require('./lib/YorickRunner')
+const { YorickRunnee } = require('./lib/YorickRunnee')
+const { RunnerConfig } = require('./lib/RunnerConfig')
 
 module.exports = {
-  YorickRunner: require('./lib/YorickRunner'),
-  YorickRunnee: require('./lib/YorickRunnee'),
-  createRunnerConfig: require('./lib/createRunnerConfig'),
+  YorickRunner,
+  YorickRunnee,
+  RunnerConfig,
 }
